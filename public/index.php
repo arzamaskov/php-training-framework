c55a4afce1673b054878fb8418e26fe332b6f3e3
Andrey Arzamaskov <arzamaskov@gmail.com>
2021-12-29 07:32:14 +0400

    fix: update index.php : include necessary files, call Router "add" method

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo __FILE__;
+$query = $_SERVER['QUERY_STRING'];
+
+require_once '../vendor/core/Router.php';
+require_once '../vendor/libs/functions.php';
+
+Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
+
+debug(Router::getRoutes());
