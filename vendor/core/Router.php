9afdeb7d1babc86703aab3d001b3ea24bed5eb10
Andrey Arzamaskov <arzamaskov@gmail.com>
2021-12-29 07:36:09 +0400

    fix: update router - create properties "route" and "routes" - create method "add" - to add route into the table of routes - create temporary methods "getRoutes" and "getRoute"

--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -2,11 +2,52 @@
 
 /**
  * Class Router.
+ * Sends requests to appropriate rout depending on parameters that
+ * contained in URL.
  */
 class Router
 {
-    public function __construct()
+    /**
+     * @array Table of all routes. By default contains only two routs.
+     * User can add themself routes.
+     */
+    protected static $routes = [];
+
+    /**
+     * @array Current route that gets by current URL.
+     */
+    protected static $route = [];
+
+    /**
+     * add.
+     *
+     * @param string $regexp Regular expression
+     * @param array  $route  route that correspond to URL
+     *
+     * @return void
+     */
+    public static function add($regexp, $route = [])
     {
-        echo 'Hello, World!';
+        self::$routes[$regexp] = $route;
+    }
+
+    /**
+     * Temporary method that returns all the routes.
+     *
+     * @return array
+     */
+    public static function getRoutes()
+    {
+        return self::$routes;
+    }
+
+    /**
+     * Temporary method that returns the current route.
+     *
+     * @return array
+     */
+    public static function getRoute()
+    {
+        return self::$route;
     }
 }
