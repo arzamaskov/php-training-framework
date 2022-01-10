5cf2c2b09fd94ac5b46862fe930a1c40c137f8c7
Andrey Arzamaskov <arzamaskov@gmail.com>
2022-01-10 12:19:14 +0400

    fix: add default routes

--- public/index.php
+++ public/index.php
@@ -5,9 +5,8 @@ $query = rtrim($_SERVER['QUERY_STRING'], '/');
 require_once '../vendor/core/Router.php';
 require_once '../vendor/libs/functions.php';
 
-Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
-Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
-Router::add('', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('([a-z-]+)/([a-z-]+)');
 
 debug(Router::getRoutes());
 
