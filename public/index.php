a9b905b0ab83aeda4b02bb5ed67e56bbf99fc1aa
Andrey Arzamaskov <arzamaskov@gmail.com>
2022-01-11 16:34:38 +0400

    fix: create classes autoloader

--- public/index.php
+++ public/index.php
@@ -14,6 +14,15 @@ $query = rtrim($_SERVER['QUERY_STRING'], '/');
 require_once '../vendor/core/Router.php';
 require_once '../vendor/libs/functions.php';
 
+// Classes autoloader
+spl_autoload_register(function ($class) {
+    $file = APP . "/controllers/$class.php";
+
+    if (is_file($file)) {
+        require_once $file;
+    }
+});
+
 Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
 Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
 
